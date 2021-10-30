b40fc6c45ff77a431bc5592ae5278d73a56681ee
joethyr <joethayr@gmail.com>
2021-10-29 21:30:57 -0600

    changed computer's initial guess for improved guessing stategies.

--- computer_code_breaker.rb
+++ computer_code_breaker.rb
@@ -3,12 +3,12 @@
 # responsible for game functionality when computer is code breaker.
 class ComputerCodeBreaker
   require_relative "game_logic"
+  require_relative "code_list"
 
   include GameLogic
   attr_reader :human_code
 
   def initialize
-
     @human_code = set_code.chars.map(&:to_i)
     puts 'Ok, computer try to crack the code!'
     computer_start
@@ -21,13 +21,10 @@ class ComputerCodeBreaker
   end
 
   def computer_start
-    p guess = initial_guess.chars.map(&:to_i)
+    p guess = 2211.digits
     check_guess(guess, human_code)
   end
 
-  def initial_guess
-    number = rand(1..6).to_s * 4
-  end
 
   def second_guess
 
